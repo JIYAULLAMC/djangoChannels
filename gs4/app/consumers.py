0fd7863804449372cccb8f447457c4565adc4bee
JIYACHINDADI <jiyachindadi@gmail.com>
2023-03-11 04:17:57 +0530

    added the async consumer and routing to the gs4

--- gs4/app/consumers.py
+++ gs4/app/consumers.py
@@ -5,20 +5,41 @@ from channels.exceptions import StopConsumer
 class MySyncConsumer(SyncConsumer):
 
     def websocket_connect(self, event):
-        print("Web socket Connect ++++++++++++++++!", event)
+        print(" SyncConsumer Web socket Connect ++++++++++++++++!", event)
         self.send({
             "type":"websocket.accept",
         })
 
     def websocket_receive(self, event):
-        print("message received from client ++++++++++",event)
+        print(" SyncConsumermessage received from client ++++++++++",event)
         print(event["text"])
         self.send({
             "type": "websocket.send",
-            "text": "message is send to sever!"
+            "text": "SyncConsumer message is send to sever!"
         })
 
     def websocket_disconnected(self, event):
-        print("disconnected! ++++++++++++++", event)
+        print(" SyncConsumer disconnected! ++++++++++++++", event)
+        raise StopConsumer() 
+
+
+class MyAsyncConsumer(AsyncConsumer):
+
+    async def websocket_connect(self, event):
+        print(" AsyncConsumer Web socket Connect ++++++++++++++++!", event)
+        await self.send({
+            "type":"websocket.accept",
+        })
+
+    async def websocket_receive(self, event):
+        print(" AsyncConsumer message received from client ++++++++++",event)
+        print(event["text"])
+        await self.send({
+            "type": "websocket.send",
+            "text": " AsyncConsumer message is send to sever!"
+        })
+
+    async def websocket_disconnected(self, event):
+        print(" AsyncConsumer disconnected! ++++++++++++++", event)
         raise StopConsumer() 
 
--- gs4/app/routing.py
+++ gs4/app/routing.py
@@ -4,4 +4,5 @@ from app import consumers
 
 websocket_urlpatterns = [
     path("ws/sc/",consumers.MySyncConsumer.as_asgi()),
+    path("ws/ac/",consumers.MyAsyncConsumer.as_asgi()),
 ]
